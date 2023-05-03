dcf42d7f4da87d2080f6855718afdef6b88768d5
ej038b8vhu <97674389+ej038b8vhu@users.noreply.github.com>
2023-05-03 22:37:46 +0800

    Update Projects.jsx

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -17,7 +17,7 @@ const projectVariant = {
 };
 
 const Project = ({ title, subtitle, intro, link }) => {
-  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-grey z-30 flex flex-col justify-center item-center text-center md:p-16 text-deep-blue`;
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-grey z-30 flex flex-col justify-center item-center text-center text-deep-blue p-3 md:p-16`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
   return (
